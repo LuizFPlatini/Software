390536eadd336a31f31639048691161c93b506ef
LuizFPlatini <luizfplatini@gmail.com>
2024-07-26 15:53:27 -0300

    Update index.php

--- index.php
+++ index.php
@@ -1,3 +1,14 @@
-<?php
-    echo "Olá, Luiz!!"
-?>
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+    <input id="input-email" type="text" placeholder= "E-mail"></input>
+    <input id="input-email" type="password" placeholder= "Senha"></input>
+    <button id='submit' onclick="submit()">Entrar</button>
+
+</body>
+</html>
